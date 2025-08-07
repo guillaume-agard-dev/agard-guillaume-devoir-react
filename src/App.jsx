bec0362547a8a03629187c2d0214f3e4ce86a0ab
Guillaume Agard <guillaumeagard.dev@gmail.com>
2025-08-07 11:27:30 +0200

    Add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './styles/App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Mentionslegales from './pages/Mentionslegales';
+
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/mentions-legales' element={<Mentionslegales />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

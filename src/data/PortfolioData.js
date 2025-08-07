e3afa0afb9c629731a17056bb5f663773e2b5800
Guillaume Agard <guillaumeagard.dev@gmail.com>
2025-08-07 22:55:00 +0200

    Add portfolio page with CSS

---
+++ src/data/PortfolioData.js
@@ -0,0 +1,49 @@
+
+// Données pour les cards du portfolio
+
+const portfolioData = [
+  {
+    title: 'Fresh Food',
+    description: 'Site de vente de produits frais en ligne',
+    img: '/images/fresh-food.jpg',
+    link:'',
+    extra: 'Site réalisé avec PHP et MySQL',
+  },
+  {
+    title: 'Restaurant Akira',
+    description: 'Site d’un restaurant japonais',
+    img: '/images/restaurant-japonais.jpg',
+    link:'',
+    extra: 'Site réalisé avec WordPress',
+  },
+  {
+    title: 'Espace bien-être',
+    description: "Site d'un centre de Spa",
+    img: '/images/espace-bien-etre.jpg',
+    link:'',
+    extra: 'Site réalisé avec LARAVEL',
+  },
+  {
+    title: 'SEO',
+    description: "Amélioration du référencement d'un site e-commerce ",
+    img: '/images/referencement.jpg',
+    link:'',
+    extra: 'Utilisation des outils SEO',
+  },
+  {
+    title: "Création d'une API",
+    description: "Création d'une API RESTFULL publique",
+    img: '/images/coder.jpg',
+    link:'',
+    extra: 'PHP - SYMFONY',
+  },
+  {
+    title: "Maquette d'un site web",
+    description: "Création du prototype d'un site",
+    img: '/images/screens.jpg',
+    link:'',
+    extra: 'Réalisé avec FIGMA',
+  },
+];
+
+export default portfolioData;
